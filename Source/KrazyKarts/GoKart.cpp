06a1a0b3c7fab66c7092316320470dbafce1501b
Kamil Waligóra <k.waligora37@gmail.com>
2021-10-12 09:41:38 +0200

    Car can move

--- Source/KrazyKarts/GoKart.cpp
+++ Source/KrazyKarts/GoKart.cpp
@@ -2,6 +2,7 @@
 
 
 #include "GoKart.h"
+#include "Engine/World.h"
 
 
 // Sets default values
@@ -9,24 +10,66 @@ AGoKart::AGoKart()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void AGoKart::BeginPlay()
 {
 	Super::BeginPlay();
+}
+// Calculate car Velocity
+void AGoKart::CalculateCarVelocity(float DeltaTime)
+{	
+	FVector Force = GetActorForwardVector() * MaxDrivingForce * Throttle;
+	Force += GetAirResistance();
+	Force += GetRollingResistance();
+	FVector Acceleration = Force / Mass;
+	Velocity += Acceleration * DeltaTime;
+}
+
+void AGoKart::UpdateLocationFromVelocity(float DeltaTime)
+{
+	FVector Translation = Velocity * 100 * DeltaTime; // *100 because meters to cm
+	FHitResult Hit;
+	AddActorWorldOffset(Translation, true, &Hit);
 	
+	// if car hit something
+	if(Hit.IsValidBlockingHit())	
+		Velocity = FVector::ZeroVector;	
+}
+
+void AGoKart::ApplyRotation(float DeltaTime, FQuat& RotationDelta)
+{
+	// Rotation angle with radians
+	float DeltaLocation = FVector::DotProduct(GetActorForwardVector(),Velocity) * DeltaTime;
+	float RotationAngle = DeltaLocation / MinTurningRadius * SteeringThrow;
+	RotationDelta = FQuat(GetActorUpVector(), RotationAngle);
+	Velocity = RotationDelta.RotateVector(Velocity);
+}
+
+// Set car throttle with player's input
+void AGoKart::Server_MoveForward_Implementation(float Value)
+{
+	Throttle = Value;
+}
+
+bool AGoKart::Server_MoveForward_Validate(float Value)
+{
+	return FMath::Abs(Value) <= 1;
 }
 
 // Called every frame
 void AGoKart::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+	Super::Tick(DeltaTime);	
+	
+	// Move car
+	CalculateCarVelocity(DeltaTime);
+	UpdateLocationFromVelocity(DeltaTime);
 
-	// MoveForward
-	FVector Translation = Veloctity * 100 * DeltaTime; // *100 because meters to cm
-	AddActorWorldOffset(Translation);
+	FQuat RotationDelta;
+	ApplyRotation(DeltaTime, RotationDelta);
+	AddActorWorldRotation(RotationDelta, true);
 }
 
 // Called to bind functionality to input
@@ -34,12 +77,29 @@ void AGoKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	PlayerInputComponent->BindAxis("MoveForward", this, &AGoKart::CalculateVelocity);
+	PlayerInputComponent->BindAxis("MoveForward", this, &AGoKart::Server_MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AGoKart::Server_MoveRight);
 }
 
-// Calculate Car's velocity
-void AGoKart::CalculateVelocity(float Value)
+void AGoKart::Server_MoveRight_Implementation(float Value)
 {
-	Veloctity = GetActorForwardVector() * 20.0f * Value; // meters/seconds
+	SteeringThrow = Value;
+}
+
+bool AGoKart::Server_MoveRight_Validate(float Value)
+{
+	return FMath::Abs(Value) <= 1;
+}
+
+FVector AGoKart::GetAirResistance()
+{
+	return -Velocity.GetSafeNormal() * Velocity.SizeSquared() * DragCoefficient;
+}
+
+FVector AGoKart::GetRollingResistance()
+{
+	float AccelerationDueToGravity  = -GetWorld()->GetGravityZ() / 100; // UE declare gravity as -980
+	float NormalForce = Mass * AccelerationDueToGravity;
+	return -Velocity.GetSafeNormal() * RollingResistanceCoefficient * NormalForce;
 }
 
--- Source/KrazyKarts/GoKart.h
+++ Source/KrazyKarts/GoKart.h
@@ -27,7 +27,39 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 private:
-	FVector Veloctity;
-	void CalculateVelocity(float Value);
+	// Mass of the car (kg)
+	UPROPERTY(EditAnywhere)
+	float Mass = 1000;
 	
+	// The force applied to the car when the throttle is fully down (N)
+	UPROPERTY(EditAnywhere)
+	float MaxDrivingForce = 10000;
+
+	// Minimum radius of the car turning circle at full lock (m)
+	UPROPERTY(EditAnywhere)
+	float MinTurningRadius = 10;
+
+	// Higher means more drag
+	UPROPERTY(EditAnywhere)
+	float DragCoefficient = 16;
+
+	// Higher means more rolling
+	UPROPERTY(EditAnywhere)
+	float RollingResistanceCoefficient = 0.015;
+	
+	FVector Velocity;
+	float Throttle;
+	float SteeringThrow;	
+
+	// Calculate car Velocity
+	void CalculateCarVelocity(float DeltaTime);
+	void UpdateLocationFromVelocity(float DeltaTime);
+	void ApplyRotation(float DeltaTime, FQuat& RotationDelta);
+	// Set car throttle with player's input
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_MoveForward(float Value);
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_MoveRight(float Value);
+	FVector GetAirResistance();
+	FVector GetRollingResistance();
 };
